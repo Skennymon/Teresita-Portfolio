14d8a26be15171a8d6615d3ad6a63ece21623821
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-09-03 21:08:10 -0700

    Added Responsive Navbar and Intro Graphic

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useState } from "react";
+import { useClickAway } from "@uidotdev/usehooks";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar() {
+  const [dropDown, setDropdown] = useState(false);
+  const ref = useClickAway(() => {
+    setDropdown(false);
+  });
+
+  return (
+    <nav className="flex justify-between p-4">
+      <h1 className="text-5xl">Teresita Portfolio</h1>
+      <div className="md:flex md:gap-4 hidden">
+        <button className="text-4xl hover:cursor-pointer">About Me</button>
+        <button className="text-4xl hvoer:cursor-pointer">My Works</button>
+      </div>
+
+      <div
+        className="p-1 rounded-md relative w-10 h-10 lg:w-32 lg:hidden md:hidden md:h-16 justify-self-end"
+        ref={ref}
+      >
+        <button
+          className="cursor-pointer"
+          onClick={() => setDropdown(!dropDown)}
+        >
+          <Image src="/hamburger.svg" alt="Hamburger Menu" fill={true} />
+        </button>
+        <ul
+          className={`flex flex-col border gap-4 w-56 absolute top-10 right-2 rounded-2xl items-center justify-center p-5 bg-gray-950 border-gray-600 shadow-gray-600 shadow transition-all duration-75 ease-in-out z-1 ${
+            dropDown
+              ? "max-h-screen opacity-100"
+              : "max-h-0 opacity-0 pointer-events-none"
+          }`}
+        >
+          <Link
+            href="/pages/about"
+            onClick={() => {
+              setDropdown(false);
+            }}
+          >
+            About me
+          </Link>
+          <Link href="/pages/contactme" onClick={() => setDropdown(false)}>
+            My Works
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+}
