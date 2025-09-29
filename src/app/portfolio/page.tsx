54d656f7440e21ed2a6a079cecb9e9270bc735a9
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-09-28 19:33:19 -0700

    Fixed Alt Issue

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -18,85 +18,110 @@ import {
 
 export default function Portfolio() {
   const photos = [
-    { src: "/cover/LatinxFashionCover.png", alt: "Latinx Fashion Cover", correspondingPhotos: [{
-      src: "/LatinxFashionPhotos/page1.png",
-      alt: "Latinx Fashion Cover Photo 1",
+    {
+      src: "/cover/LatinxFashionCover.png",
+      alt: "Latinx Fashion Cover",
+      correspondingPhotos: [
+        {
+          src: "/LatinxFashionPhotos/page1.png",
+          alt: "Latinx Fashion Cover Photo 1",
+        },
+        {
+          src: "/LatinxFashionPhotos/page2.png",
+          alt: "Latinx Fashion Cover Photo 2",
+        },
+        {
+          src: "/LatinxFashionPhotos/page3.png",
+          alt: "Latinx Fashion Cover Photo 3",
+        },
+        {
+          src: "/LatinxFashionPhotos/page4.png",
+          alt: "Latinx Fashion Cover Photo 4",
+        },
+        {
+          src: "/LatinxFashionPhotos/page5.png",
+          alt: "Latinx Fashion Cover Photo 5",
+        },
+        {
+          src: "/LatinxFashionPhotos/page6.png",
+          alt: "Latinx Fashion Cover Photo 6",
+        },
+        {
+          src: "/LatinxFashionPhotos/page7.png",
+          alt: "Latinx Fashion Cover Photo 7",
+        },
+        {
+          src: "/LatinxFashionPhotos/page8.png",
+          alt: "Latinx Fashion Cover Photo 8",
+        },
+        {
+          src: "/LatinxFashionPhotos/page9.png",
+          alt: "Latinx Fashion Cover Photo 9",
+        },
+        {
+          src: "/LatinxFashionPhotos/page10.png",
+          alt: "Latinx Fashion Cover Photo 10",
+        },
+        {
+          src: "/LatinxFashionPhotos/page11.png",
+          alt: "Latinx Fashion Cover Photo 11",
+        },
+        {
+          src: "/LatinxFashionPhotos/page12.png",
+          alt: "Latinx Fashion Cover Photo 12",
+        },
+        {
+          src: "/LatinxFashionPhotos/page13.png",
+          alt: "Latinx Fashion Cover Photo 13",
+        },
+        {
+          src: "/LatinxFashionPhotos/page14.png",
+          alt: "Latinx Fashion Cover Photo 14",
+        },
+        {
+          src: "/LatinxFashionPhotos/page15.png",
+          alt: "Latinx Fashion Cover Photo 15",
+        },
+        {
+          src: "/LatinxFashionPhotos/page16.png",
+          alt: "Latinx Fashion Cover Photo 16",
+        },
+        {
+          src: "/LatinxFashionPhotos/page17.png",
+          alt: "Latinx Fashion Cover Photo 17",
+        },
+      ],
     },
     {
-      src: "/LatinxFashionPhotos/page2.png",
-      alt: "Latinx Fashion Cover Photo 2",
+      src: "/cover/GravitiFitnessCover.jpg",
+      alt: "Graviti Fitness Cover",
+      correspondingPhotos: [{ src: "/LatinxFashionPhotos/page16.png", alt: "Alt" }],
     },
     {
-      src: "/LatinxFashionPhotos/page3.png",
-      alt: "Latinx Fashion Cover Photo 3",
+      src: "/cover/ZotfestCover.jpg",
+      alt: "Zotfest Cover",
+      correspondingPhotos: [],
     },
     {
-      src: "/LatinxFashionPhotos/page4.png",
-      alt: "Latinx Fashion Cover Photo 4",
+      src: "/cover/DreamBoyCover.png",
+      alt: "DreamBoys Cover",
+      correspondingPhotos: [],
     },
     {
-      src: "/LatinxFashionPhotos/page5.png",
-      alt: "Latinx Fashion Cover Photo 5",
+      src: "/cover/EcholaliaDreams.jpg",
+      alt: "Echolalia Dreams Cover",
+      correspondingPhotos: [],
     },
-    {
-      src: "/LatinxFashionPhotos/page6.png",
-      alt: "Latinx Fashion Cover Photo 6",
-    },
-    {
-      src: "/LatinxFashionPhotos/page7.png",
-      alt: "Latinx Fashion Cover Photo 7",
-    },
-    {
-      src: "/LatinxFashionPhotos/page8.png",
-      alt: "Latinx Fashion Cover Photo 8",
-    },
-    {
-      src: "/LatinxFashionPhotos/page9.png",
-      alt: "Latinx Fashion Cover Photo 9",
-    },
-    {
-      src: "/LatinxFashionPhotos/page10.png",
-      alt: "Latinx Fashion Cover Photo 10",
-    },
-    {
-      src: "/LatinxFashionPhotos/page11.png",
-      alt: "Latinx Fashion Cover Photo 11",
-    },
-    {
-      src: "/LatinxFashionPhotos/page12.png",
-      alt: "Latinx Fashion Cover Photo 12",
-    },
-    {
-      src: "/LatinxFashionPhotos/page13.png",
-      alt: "Latinx Fashion Cover Photo 13",
-    },
-    {
-      src: "/LatinxFashionPhotos/page14.png",
-      alt: "Latinx Fashion Cover Photo 14",
-    },
-    {
-      src: "/LatinxFashionPhotos/page15.png",
-      alt: "Latinx Fashion Cover Photo 15",
-    },
-    {
-      src: "/LatinxFashionPhotos/page16.png",
-      alt: "Latinx Fashion Cover Photo 16",
-    },
-    {
-      src: "/LatinxFashionPhotos/page17.png",
-      alt: "Latinx Fashion Cover Photo 17",
-    },
-    ] },
-    { src: "/cover/GravitiFitnessCover.jpg", alt: "Graviti Fitness Cover", correspondingPhotos: [] },
-    { src: "/cover/ZotfestCover.jpg", alt: "Zotfest Cover", correspondingPhotos: [] },
-    { src: "/cover/DreamBoyCover.png", alt: "DreamBoys Cover",correspondingPhotos: [] },
-    { src: "/cover/EcholaliaDreams.jpg", alt: "Echolalia Dreams Cover", correspondingPhotos: [] },
     {
       src: "/cover/Fashion&SneakersCover.jpg",
       alt: "Fashion & Sneakers Cover",
       correspondingPhotos: [],
     },
-    { src: "/cover/LeyMusicVideoCover.jpg", alt: "Ley Music Video Cover", correspondingPhotos: [] },
+    {
+      src: "/cover/LeyMusicVideoCover.jpg",
+      alt: "Ley Music Video Cover",
+      correspondingPhotos: [],
+    },
     {
       src: "/cover/EcholaliaDreamsSetCover.jpg",
       alt: "Echolalia Dreams Set Cover",
@@ -108,80 +133,6 @@ export default function Portfolio() {
       correspondingPhotos: [],
     },
   ];
-
-  /*
-  const correspondingPhotos = [
-    {
-      src: "/LatinxFashionPhotos/page1.png",
-      alt: "Latinx Fashion Cover Photo 1",
-    },
-    {
-      src: "/LatinxFashionPhotos/page2.png",
-      alt: "Latinx Fashion Cover Photo 2",
-    },
-    {
-      src: "/LatinxFashionPhotos/page3.png",
-      alt: "Latinx Fashion Cover Photo 3",
-    },
-    {
-      src: "/LatinxFashionPhotos/page4.png",
-      alt: "Latinx Fashion Cover Photo 4",
-    },
-    {
-      src: "/LatinxFashionPhotos/page5.png",
-      alt: "Latinx Fashion Cover Photo 5",
-    },
-    {
-      src: "/LatinxFashionPhotos/page6.png",
-      alt: "Latinx Fashion Cover Photo 6",
-    },
-    {
-      src: "/LatinxFashionPhotos/page7.png",
-      alt: "Latinx Fashion Cover Photo 7",
-    },
-    {
-      src: "/LatinxFashionPhotos/page8.png",
-      alt: "Latinx Fashion Cover Photo 8",
-    },
-    {
-      src: "/LatinxFashionPhotos/page9.png",
-      alt: "Latinx Fashion Cover Photo 9",
-    },
-    {
-      src: "/LatinxFashionPhotos/page10.png",
-      alt: "Latinx Fashion Cover Photo 10",
-    },
-    {
-      src: "/LatinxFashionPhotos/page11.png",
-      alt: "Latinx Fashion Cover Photo 11",
-    },
-    {
-      src: "/LatinxFashionPhotos/page12.png",
-      alt: "Latinx Fashion Cover Photo 12",
-    },
-    {
-      src: "/LatinxFashionPhotos/page13.png",
-      alt: "Latinx Fashion Cover Photo 13",
-    },
-    {
-      src: "/LatinxFashionPhotos/page14.png",
-      alt: "Latinx Fashion Cover Photo 14",
-    },
-    {
-      src: "/LatinxFashionPhotos/page15.png",
-      alt: "Latinx Fashion Cover Photo 15",
-    },
-    {
-      src: "/LatinxFashionPhotos/page16.png",
-      alt: "Latinx Fashion Cover Photo 16",
-    },
-    {
-      src: "/LatinxFashionPhotos/page17.png",
-      alt: "Latinx Fashion Cover Photo 17",
-    },
-    
-  ];
-  */
 
   return (
     <>
@@ -207,13 +158,14 @@ export default function Portfolio() {
 
               <Carousel className="w-full flex items-center justify-center">
                 <CarouselContent>
-                  {photos.map((photo) => (
-                    photo.correspondingPhotos.map((correspondingPhoto) => (
-                        <CarouselItem key={correspondingPhoto.src} className="w-full flex items-center justify-center">
-                            <Image src={correspondingPhoto.src} alt={correspondingPhoto.alt} width={800} height={800} quality={80}></Image>
-                        </CarouselItem>
-                    )   
-                    )))}
+                  {photo.correspondingPhotos.map((correspondingPhoto) => (
+                    <CarouselItem
+                      key={correspondingPhoto.src}
+                      className="w-full flex items-center justify-center"
+                      >
+                        <Image src={correspondingPhoto.src} alt={correspondingPhoto.alt} width={1000} height={1000} quality={80}></Image>
+                    </CarouselItem>
+                  ))}
                 </CarouselContent>
                 <CarouselPrevious className="ml-8" />
                 <CarouselNext className="mr-8" />
