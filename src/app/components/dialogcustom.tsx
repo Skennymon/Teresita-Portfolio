7ce41e4c164c2a30d32bc2094583e5ae55784c9f
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-10-01 02:09:35 -0700

    Fixed Overflow Bug

--- src/app/components/dialogcustom.tsx
+++ src/app/components/dialogcustom.tsx
@@ -29,7 +29,10 @@ export default function DialogCustom({
 }) {
   return (
     <Dialog>
-      <DialogTrigger key={src} className="cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+      <DialogTrigger
+        key={src}
+        className="cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+      >
         <Image
           src={src}
           alt={alt}
@@ -39,32 +42,31 @@ export default function DialogCustom({
           className="transition duration 300 ease-in-out hover:-translate-y-1 hover:scale-100"
         ></Image>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-full md:max-w-3xl lg:max-w-4xl xl:max-w-5xl sm:max-h-full md:max-h-2xl lg:max-h-3xl xl:max-h-5xl">
+      <DialogContent className="h-auto w-auto md:w-[700px] lg:w-[1000px] xl:w-[2000px]">
         <DialogHeader>
           <DialogTitle className="text-2xl font-bold">{alt}</DialogTitle>
         </DialogHeader>
 
-        <Carousel className="w-full flex items-center justify-center">
+        <Carousel className="w-full h-full flex items-center justify-center">
           <CarouselContent>
             {correspondingPhotos.map((correspondingPhoto) => (
               <CarouselItem
                 key={correspondingPhoto.src}
-                className="w-full flex items-center justify-center"
+                className="w-full h-full flex items-center justify-center"
               >
                 <Image
                   src={correspondingPhoto.src}
                   alt={correspondingPhoto.alt}
-                  width={700}
-                  height={200}
-                  quality={80}
+                  width={900}
+                  height={700}
                 ></Image>
               </CarouselItem>
             ))}
             {correspondingVideos.map((correspondingVideo) => (
               <iframe
                 key={correspondingVideo.src}
-                width={950}
-                height={600}
+                width={600}
+                height={500}
                 src={correspondingVideo.src}
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
