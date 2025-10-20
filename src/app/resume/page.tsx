69ad241c38042de34851dbc0b34fc0be1ba44b20
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-10-19 19:15:58 -0700

    Fixed Resume Page Blocking Navbar on Smaller Screens

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Resume() {
   return (
-    <div className="flex w-full p-8 items-center justify-center">
+    <div className="flex w-full p-8 items-center justify-center md:mt-36">
       <Image src="/TeresitaResume_page-0001.jpg" alt="Resume" width={1000} height={1000} className="rounded-lg"/>
     </div>
   );
