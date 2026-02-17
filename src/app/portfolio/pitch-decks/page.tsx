b46bb1a465b8cc2e89d2cb5898168810e00c9fb5
Skennymon <77165403+Skennymon@users.noreply.github.com>
2026-02-16 19:51:00 -0800

    Finished Pitch Deck Page

--- src/app/portfolio/pitch-decks/page.tsx
+++ src/app/portfolio/pitch-decks/page.tsx
@@ -5,7 +5,9 @@ export default function PitchDecks() {
         <>
             <WorkNavbar current="pitch-decks"/>
             <h1 className="text-5xl items-center justify-center flex font-pt-serif">My Pitchdecks</h1>
-            <div className="flex items-center justify-center">
+            
+            <div className="flex flex-col items-center justify-center mt-12">
+                <a className="text-4xl items-center justify-center flex font-pt-serif hover:text-red-500 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-110 active:scale-y-90 hover:-translate-y-1 hover:scale-105 hover:cursor-pointer" href="https://www.canva.com/design/DAHA9XQwJpg/Ubhnjrs7mPfgl_CJjHGXoA/view?embed" target="_blank">Gentle Monster x Bon Bon</a>
                 <iframe className="w-3/4 h-screen" src="https://www.canva.com/design/DAHA9XQwJpg/Ubhnjrs7mPfgl_CJjHGXoA/view?embed" allowFullScreen></iframe>
             </div>
         </>
